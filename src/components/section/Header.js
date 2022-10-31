5e9fd4c24fbc0c493002ef0a7361720a746b44d7
mrshafqatnadeem <shafqatnadeem2@gmail.com>
2022-11-01 02:35:37 +0500

    changes

--- src/components/section/Header.js
+++ src/components/section/Header.js
@@ -31,6 +31,7 @@ const Header = () => {
         <div>
             <div className="Header">
                 <h1>{ `I'm ${header.name}` }</h1>
+                <p>{header.title}</p>
             <p className="line-1 anim-typewriter">and this is my portfolio... </p>
             <label className="switch">
                 <input id="mode-switch" onClick={e => toggleDarkMode(e)} type="checkbox"/>
