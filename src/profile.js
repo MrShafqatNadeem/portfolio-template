cab48dcf19720b1336988ee04c152295ebe392f1
mrshafqatnadeem <shafqatnadeem2@gmail.com>
2022-12-16 16:03:22 +0500

    Revert "Update profile.js"
    This reverts commit 45d3d97530546235cde15585410d2d74a59f81ee.

--- src/profile.js
+++ src/profile.js
@@ -89,42 +89,42 @@ const projects = [
     id: "project1",
     name: "Project 1",
     skills: ["HTML, CSS, JS"],
-    url: "https://github.com/MrShafqatNadeem",
+    url: "https://github.com/kaustubhai",
   },
   {
     // Add image in './styles/images.css' in #project2
     id: "project2",
     name: "Project 2",
     skills: ["HTML, CSS, JS"],
-    url: "https://github.com/MrShafqatNadeem",
+    url: "https://github.com/kaustubhai",
   },
   {
     // Add image in './styles/images.css' in #project3
     id: "project3",
     name: "Project 3",
     skills: ["HTML, CSS, JS"],
-    url: "https://github.com/MrShafqatNadeem",
+    url: "https://github.com/kaustubhai",
   },
   {
     // Add image in './styles/images.css' in #project4
     id: "project4",
     name: "Project 4",
     skills: ["HTML, CSS, JS"],
-    url: "https://github.com/MrShafqatNadeem",
+    url: "https://github.com/kaustubhai",
   },
   {
     // Add image in './styles/images.css' in #project5
     id: "project5",
     name: "Project 5",
     skills: ["HTML, CSS, JS"],
-    url: "https://github.com/MrShafqatNadeem",
+    url: "https://github.com/kaustubhai",
   },
   {
     // Add image in './styles/images.css' in #project6
     id: "project6",
     name: "Project 6",
     skills: ["HTML, CSS, JS"],
-    url: "https://github.com/MrShafqatNadeem",
+    url: "https://github.com/kaustubhai",
   },
 ];
 // Edit your Miscellaneous Activities, its name and the url.
@@ -136,19 +136,19 @@ const miscellaneous = [
     // Add image in './styles/images.css' in #misc1
     id: "misc1",
     name: "Miscellaneous 1",
-    url: "https://github.com/MrShafqatNadeem",
+    url: "https://github.com/kaustubhai",
   },
   {
     // Add image in './styles/images.css' in #misc2
     id: "misc2",
     name: "Miscellaneous 2",
-    url: "https://github.com/MrShafqatNadeem",
+    url: "https://github.com/kaustubhai",
   },
   {
     // Add image in './styles/images.css' in #misc3
     id: "misc3",
     name: "Miscellaneous 3",
-    url: "https://github.com/MrShafqatNadeem",
+    url: "https://github.com/kaustubhai",
   },
 ];
 // Contact form text, and Formspree link(to send a submit contact through their API as in contact.js)
@@ -158,7 +158,7 @@ const section5Title = "Get in Touch";
 const contact = {
   pitch:
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mauris nunc congue nisi vitae.",
-  copyright: "Shafqat Nadeem",
+  copyright: "Kaustubh Mishra",
   contactUrl: "",
 };
 // Paste your respective social media links. You can omit any if you dont have it
